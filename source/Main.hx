411d892e4114661c3c498c43bcf5a0d57419b360
tyindev <168133165+tyindev@users.noreply.github.com>
2024-06-23 14:40:35 -0400

    obs detection

--- source/Main.hx
+++ source/Main.hx
@@ -2,8 +2,12 @@ package;
 
 import flixel.FlxGame;
 import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.utils.Timer;
+import openfl.events.TimerEvent;
 import sys.io.File;
 import sys.FileSystem;
+import sys.io.Process;
 import states.PlayState;
 import states.SayoriQuickEndState;
 
@@ -27,6 +31,19 @@ class Main extends Sprite
         addChild(new FlxGame(config.width, config.height, initialState, 
             #if (flixel < "5.0.0") config.zoom, #end 
             config.framerate, config.framerate, config.skipSplash, config.startFullscreen));
+
+        // There's probably a way better way to do this but this works so yeah
+        var timer = new Timer(1000); // checking for obs every second xd
+        timer.addEventListener(TimerEvent.TIMER, onCheckObs);
+        timer.start();
+
+        if (!isObsRunning()) {
+            trace("OBS is not running!");
+        }
+        else
+        {
+            trace("OBS is started and running!");
+        }
     }
 
     private function checkFiles():Bool
@@ -41,7 +58,6 @@ class Main extends Sprite
            deleteDirRecursively("assets/characters");
            trace("deleted characters folder lol!");
            charsexist = false;
-
         }
 
         if (!sayoriExists) {
@@ -61,17 +77,43 @@ class Main extends Sprite
     // Credits: https://ashes999.github.io/learnhaxe/recursively-delete-a-directory-in-haxe.html
     private function deleteDirRecursively(path:String) : Void
     {
-    if (sys.FileSystem.exists(path) && sys.FileSystem.isDirectory(path))
-    {
-        var entries = sys.FileSystem.readDirectory(path);
-        for (entry in entries) {
-        if (sys.FileSystem.isDirectory(path + '/' + entry)) {
-            deleteDirRecursively(path + '/' + entry);
-            sys.FileSystem.deleteDirectory(path + '/' + entry);
-        } else {
-            sys.FileSystem.deleteFile(path + '/' + entry);
-        }
+        if (sys.FileSystem.exists(path) && sys.FileSystem.isDirectory(path))
+        {
+            var entries = sys.FileSystem.readDirectory(path);
+            for (entry in entries) {
+                if (sys.FileSystem.isDirectory(path + '/' + entry)) {
+                    deleteDirRecursively(path + '/' + entry);
+                    sys.FileSystem.deleteDirectory(path + '/' + entry);
+                } else {
+                    sys.FileSystem.deleteFile(path + '/' + entry);
+                }
+            }
         }
     }
+
+    private function isObsRunning():Bool
+    {
+        var result = "";
+        try {
+            var process = new Process("tasklist", []);
+            var output = process.stdout.readAll().toString();
+            result = output.toLowerCase();
+        } catch (e:Dynamic) {
+            trace("Failed to check running processes: " + e);
+            return false;
+        }
+
+        return result.indexOf("obs64.exe") != -1 || result.indexOf("obs32.exe") != -1;
+    }
+
+    private function onCheckObs(event:TimerEvent):Void
+    {
+        if (!isObsRunning()) {
+            trace("OBS is not running!");
+        }
+        else
+        {
+            trace("OBS started and running!");
+        }
     }
 }
